09ca30f6f74a4defb45c34f912f60f842d4668e0
Trust <okpusjamin@gmail.com>
2022-12-01 21:18:32 +0100

    chore: update linters and husky

--- lint-staged.js
+++ lint-staged.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "*.{ts,tsx}": ["lint --max-warnings=0"],
-  "*.{ts,tsx,json,css,ts}": ["prettier --write"],
+  "*.{js,jsx,ts,tsx}": ["eslint --max-warnings=0", () => "tsc-files --noEmit"],
+  "*.{js,jsx,ts,tsx,json,css,js}": ["prettier --write"],
 };
